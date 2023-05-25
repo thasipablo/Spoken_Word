fff8c7be3ad89666d96438c262bd2b08b403165c
thasipablo <thasipablo@gmail.com>
2023-05-25 12:33:38 -0400

    add router file and render home page with it

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { RouterProvider } from "react-router-dom"
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { router } from './router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
