9757ed1a3bd4b52313d37f9db3894b4b5b1569d3
thasipablo <thasipablo@gmail.com>
2023-05-30 12:14:05 -0400

    install headless ui and use template

--- src/pages/layouts/LeftZone.jsx
+++ src/pages/layouts/LeftZone.jsx
@@ -1,7 +1,21 @@
 const LeftZone = () => {
   return (
-    <div>LeftZone</div>
-  )
-}
+    <section className="border-r">
+      <a href="#">
+        <h1>The Spoken word</h1>
+      </a>
+      <nav>
+        <ul className="list-none">
+          <li><a href="#">Latest</a></li>
+          <li><a href="#">Series</a></li>
+          <li><a href="#">Single</a></li>
+          <li><a href="#">Consecration</a></li>
+          <li><a href="#">Monday</a></li>
+          <li><a href="#">Melodies</a></li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
 
-export default LeftZone
+export default LeftZone;
